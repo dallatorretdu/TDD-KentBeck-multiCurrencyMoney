4b7fc2696939fbc20dc58653834e3a48f6166249
Pietro Di Bello <orione-team+dip@xpeppers.com>
2015-02-19 12:48:19 +0100

    removed subclasses from money

--- src/xpeppers/training/tdd/Money.java
+++ src/xpeppers/training/tdd/Money.java
@@ -1,26 +1,43 @@
 package xpeppers.training.tdd;
 
-abstract class Money {
+
+ class Money {
 	
 	protected int amount;
 	protected String currency;
 	
+	public Money(int amount, String currency){
+		this.amount = amount;
+		this.currency = currency;
+	}
+	
 	public boolean equals(Object object){
 		Money money = (Money) object;
 		return amount == money.amount
-				&& getClass().equals(money.getClass());
+			&& currency == money.currency;
 	}
 	
 	static Money dollar(int amount){
-		return new Dollar(amount, "USD");
+		return new Money(amount, "USD");
 	}
 	static Money franc(int amount){
-		return new Franc(amount, "CHF");
+		return new Money(amount, "CHF");
 	}
 	
-	abstract Money times(int multiplier);
+	public Money times(int multiplier){
+		return new Money(amount * multiplier, currency);
+	}
 
 	public String currency() {
 		return currency;
 	}
+	
+	public String toString() {
+		return amount + " " + currency;
+	}
+
+	public Money plus(Money addend) {
+		return new Money(this.amount + addend.amount, this.currency);
+	}
+	
 }
--- test/xpeppers/training/tdd/MoneyTest.java
+++ test/xpeppers/training/tdd/MoneyTest.java
@@ -14,9 +14,11 @@ public class MoneyTest {
 	};
 	
 	@Test
-	public void testEqualityOnDollars() {
+	public void testEqualityOnMoney() {
 		assertTrue(Money.dollar(5).equals( Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals( Money.dollar(12)));
+		assertTrue(Money.franc(5).equals( Money.franc(5)));
+		assertFalse(Money.franc(5).equals( Money.franc(12)));
 	};
 	
 	@Test	
@@ -27,20 +29,29 @@ public class MoneyTest {
 	};
 	
 	@Test
-	public void testEqualityOnFrancs() {
-		assertTrue(Money.dollar(5).equals( Money.dollar(5)));
-		assertFalse(Money.dollar(5).equals( Money.dollar(12)));
-	};
-	
-	@Test
 	public void testEqualityBetweenCurrenciesFails() {
 		assertFalse(Money.dollar(5).equals( Money.franc(5)));
-	}; 
+	};
 	
 	@Test
 	public void testCurrencies() {
 		assertEquals("USD", Money.dollar(1).currency());
 		assertEquals("CHF", Money.franc(1).currency());
+	};
+	
+	@Test
+	public void testAddition() {
+		assertEquals(Money.dollar(15), Money.dollar(5).plus(Money.dollar(10)));
+		assertEquals(Money.franc(15), Money.franc(10).plus(Money.franc(5)));
+	};
+	
+	@Test
+	public void testAdditionOnDifferentCurrencies() {
+		Money dollars = Money.dollar(5);
+		Money francs = Money.franc(10);
+		Money total = dollars.plus(francs);
+		assertEquals(Money.dollar(10) , total);
+		
 	};
 	
 }
